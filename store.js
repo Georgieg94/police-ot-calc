ffbbd562030d8d6efebd403f0be80630c326bbb7
Georgieg94 <georgiegriffin94@aol.com>
2025-12-17 10:42:24 +0000

    store.js

--- store.js
+++ store.js
@@ -1,5 +1,3 @@
-const STORE_KEY = 'police_ot_entries';
-
 function getEntries() {
   return JSON.parse(localStorage.getItem(STORE_KEY) || '[]');
 }
@@ -11,9 +9,9 @@ function saveEntry(entry) {
 }
 
 function fileToBase64(file) {
-  return new Promise(res => {
-    const r = new FileReader();
-    r.onload = () => res(r.result);
-    r.readAsDataURL(file);
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.readAsDataURL(file);
   });
 }
