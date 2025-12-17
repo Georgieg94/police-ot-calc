e5e4942314bc6071f59d0c0022bd3b9af2821f0a
Georgieg94 <georgiegriffin94@aol.com>
2025-12-17 10:38:28 +0000

    export.js

--- export.js
+++ export.js
@@ -1,1 +1,12 @@
-
+function exportCalendar(entries) {
+  let ics = 'BEGIN:VCALENDAR\nVERSION:2.0\n';
+  entries.forEach(e => {
+    const d = new Date(e.date).toISOString().replace(/[-:]/g,'').split('.')[0];
+    ics += `BEGIN:VEVENT\nDTSTART:${d}\nSUMMARY:Police Overtime\nEND:VEVENT\n`;
+  });
+  ics += 'END:VCALENDAR';
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(new Blob([ics], {type:'text/calendar'}));
+  a.download = 'police-ot.ics';
+  a.click();
+}
