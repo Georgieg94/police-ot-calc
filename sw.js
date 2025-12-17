2624d4ae7c9a73633bbafcb910ec64c911655890
Georgieg94 <georgiegriffin94@aol.com>
2025-12-17 14:39:41 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,35 @@
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open('police-ot').then(c=>c.addAll(['./'])));
+const CACHE_NAME = 'police-ot-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request)
+    )
+  );
 });
