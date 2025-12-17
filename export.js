eb120e4c3de3c35af3dd6537e4e73257d6e5ae02
Georgieg94 <georgiegriffin94@aol.com>
2025-12-17 14:10:31 +0000

    export.js

--- export.js
+++ export.js
@@ -1,12 +1,16 @@
-function exportCalendar(entries) {
-  let ics = 'BEGIN:VCALENDAR\nVERSION:2.0\n';
-  entries.forEach(e => {
-    const d = new Date(e.date).toISOString().replace(/[-:]/g,'').split('.')[0];
-    ics += `BEGIN:VEVENT\nDTSTART:${d}\nSUMMARY:Police Overtime\nEND:VEVENT\n`;
-  });
-  ics += 'END:VCALENDAR';
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(new Blob([ics], {type:'text/calendar'}));
-  a.download = 'police-ot.ics';
-  a.click();
-}
+exportCal.onclick=()=>{
+const entries=storeGet();
+if(!entries.length) return alert('No entries');
+let ics='BEGIN:VCALENDAR\nVERSION:2.0\n';
+entries.forEach(e=>{
+const d=new Date(e.date).toISOString().replace(/[-:]/g,'').split('.')[0];
+ics+=`BEGIN:VEVENT\nDTSTART:${d}\nSUMMARY:Police OT ${e.hours}h\nEND:VEVENT\n`;
+});
+ics+='END:VCALENDAR';
+const a=document.createElement('a');
+a.href=URL.createObjectURL(new Blob([ics],{type:'text/calendar'}));
+a.download='police-ot.ics';
+a.click();
+};
+
+exportPdf.onclick=()=>alert('PDF layout coming next drop');
